35b77c6c3fdfc5f49693cfb06436f55013a581dd
Vien Van <vienvonvan@gmail.com>
2016-01-26 22:06:40 -0800

    added post counter

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 console.log('linked');
 
-var button = document.getElementById('changeText');
+var button = document.getElementById('postBtn');
 var posts = [];
+var postCounter = 0;
 var ul = document.getElementById('blog-list');
+var h2 = document.getElementById('postCounter');
+
 function addPostToArray() {
       var blogPost = document.getElementById('blog').value;
       posts.push(blogPost);
 }
 
 function createList() {
-  for (var i = 0; i < posts.length; i++) {
-    ul.innerHTML += "<li>" + posts[i] + "</li>";
-    console.log(posts[i]);
+    for (var i = 0; i < posts.length; i++) {
+      ul.innerHTML += "<li>" + posts[i] + "</li>";
+      postCounter++;
   }
+  h2.innerHTML = postCounter;
 }
 
-button.addEventListener('click', function() {
+button.addEventListener('click', function(e) {
+      e.preventDefault();
       addPostToArray();
       createList();
 })
