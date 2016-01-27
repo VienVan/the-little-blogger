a72375b6f3355c8a01946499b8ea73310572318b
Vien Van <vienvonvan@gmail.com>
2016-01-26 21:46:56 -0800

    added input value to lists

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 console.log('linked');
 
 var button = document.getElementById('changeText');
-var blogPost = document.getElementById('blog').value;
 var posts = [];
-
-function addPosts() {
-  var blogPost = document.getElementById('blog').value;
-  posts.push(blogPost);
+var ul = document.getElementById('blog-list');
+function addPostToArray() {
+      var blogPost = document.getElementById('blog').value;
+      posts.push(blogPost);
 }
+
+function createList() {
+  for (var i = 0; i < posts.length; i++) {
+    ul.innerHTML += "<li>" + posts[i] + "</li>";
+    console.log(posts[i]);
+  }
+}
+
 button.addEventListener('click', function() {
-  addPosts();
+      addPostToArray();
+      createList();
 })
